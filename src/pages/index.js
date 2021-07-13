45613d0bb3a5f3fba1d0d2bc1a0eca8ab4b9e80c
lilman109 <akira.yoshidasjsu@gmail.com>
2021-07-13 22:35:10 +0900

    added banner carousel

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,19 @@
 import { bindActionCreators } from '@reduxjs/toolkit';
 import Head from 'next/head';
-import { Header } from '../components/index';
+import { Header, Banner } from '../components/';
 
 export default function Home() {
 	return (
-		<div>
+		<div className='bg-gray-100'>
 			<Head>
 				<title>Amazon 2.0</title>
 			</Head>
 			<Header />
+
+			<main>
+				{/* banner */}
+				<Banner />
+			</main>
 		</div>
 	);
 }
